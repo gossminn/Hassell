2c3f237843775eb331003c3e427f97658b77e8bf
gossminn <gosseminnema@gmail.com>
2017-05-31 10:25:25 +0200

    Updated bifurcation diagram (now has two lines if there are two limits)

--- hassell.fsx
+++ hassell.fsx
@@ -13,14 +13,18 @@ let b = 4.0
 // Hassell equation (as function with x_{t-1} and lambda as parameters)
 let hassell prev lambda = (lambda * prev) / ((1.0 + alpha * prev) ** b)
 
-// Approximate the limit of x_t (apply Hassell function 1000 times)
-let approximateLimit start lambda = 
-    let limit = 1000
+// Calculate population at time t (apply Hassell function t times)
+let populationAt start time lambda = 
     let rec helper count x = 
-        if count >= limit then x
+        if count = time then x
         else helper (count + 1) (hassell x lambda)
     helper 0 start
 
-// Draw bifurcation diagram for 0 <= lambda <= 100
-{0.0 .. 100.0} |> Seq.map (approximateLimit 1.0) |> Chart.Line
+// Partially applied functions for approximating limit (999 and 1000 times)
+let approxLimit1000 = populationAt 1.0 1000
+let approxLimit999 = populationAt 1.0 999
 
+// Draw bifurcation diagrams for 0 <= lambda <= 999 and 0 <= lambda <= 999
+let line1 = [0.0 .. 100.0] |> List.map approxLimit1000 |> Chart.Line
+let line2 = [0.0 .. 100.0] |> List.map approxLimit999 |> Chart.Line
+[line1; line2] |> Chart.Combine
